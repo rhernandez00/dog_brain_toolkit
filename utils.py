a2de6c64efb5184752e66a5400dd906a6d2b963d
Raúl Hernández <raulhdatascientist@gmail.com>
2024-03-20 18:09:39 +0100

    added extract_params

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 
 import os
 import shutil
+import subprocess
 
 def fill_fsf(to_fill_dict, design_path, design_modified_path):
     """"
@@ -41,3 +42,48 @@ def fill_fsf(to_fill_dict, design_path, design_modified_path):
         file.writelines(modified_lines)
     # print name of output file
     print('Output file: ' + design_modified_path)
+
+def extract_params(input_file):
+    """
+    Gets the TR and number of volumes of a file using fslinfo
+    
+    Parameters:
+    - input_file (str): The path to the input file for the fslinfo command.
+    
+    Returns:
+    TR - TR
+    volumes - number of volumes
+    
+    """
+    # Construct the command to run
+    command = f"fslinfo {input_file}"
+    
+    # Run the command and capture the output
+    try:
+        # subprocess.check_output returns the output of the command
+        output = subprocess.check_output(command, shell=True, text=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to run '{command}': {e}")
+        return None
+    
+    # Split the output into lines
+    lines = output.split('\n')
+    
+    # Loop through each line
+    for line in lines:
+        # Check if the line contains 'dim4'
+        if 'dim4' in line:
+            # Split the line by spaces and extract the value after 'dim4'
+            parts = line.split()
+            # Assuming the value is always after 'dim4', which is at index 1
+            volumes = int(parts[1])  # Convert the value to float
+            break
+    for line in lines:
+        if 'pixdim4' in line:
+            # Split the line by spaces and extract the value after 'pixdim4'
+            parts2 = line.split()
+            # Assuming the value is always after 'dim4', which is at index 1
+            TR = float(parts2[1])  # Convert the value to float
+            break
+    
+    return TR,volumes
