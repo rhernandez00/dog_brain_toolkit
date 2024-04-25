663d5794113bc0cabe54df7819ef9910cbbce3cb
Raúl Hernández <raulhdatascientist@gmail.com>
2024-04-25 06:22:53 +0200

    added run_bet

--- run_bet.sh
+++ run_bet.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
 # This script runs 1 to 3 BET based on input coordinates for the spheres
-# files go without extension
-# Usage: ./run_bet.sh <inputfile> <outputfile> <thr1> <x1> <y1> <z1> <thr2> <x2> <y2> <z2> <thr3> <x3> <y3> <z3> 
+# Usage: ./run_bet.sh <inputfile> <outputfile> <paramsfile>
 
 # Author: Raul Hernandez
 
 inputfile=$1
 outputfile=$2
+paramsfile=$3
 
-thr1=$3
-x1=$4
-y1=$5
-z1=$6
-thr2=$7
-x2=$8
-y2=$9
-z2=$10
-thr3=$11
-x3=$12
-y3=$13
-z3=$14
+# read paramsfile and load variables in the file, will read:
+# betx_1, bety_1, betz_1, thr_1, betx_2, bety_2, betz_2, thr_2, betx_3, bety_3, betz_3, thr_3
+source ${paramsfile}
 
 # get working folder
 workingFolder=$(dirname ${outputfile})
 
-bet ${inputfile} ${workingFolder}\mask1.nii.gz -f ${thr1} -g 0 -c ${x1} ${y1} ${z1} -m
-cp ${workingFolder}\mask1.nii.gz ${outputfile}
+bet ${inputfile} ${workingFolder}/mask1.nii.gz -f ${thr_1} -g 0 -c ${betx_1} ${bety_1} ${betz_1} -m
+cp ${workingFolder}/mask1.nii.gz ${outputfile}
 
 # if thr2 > 0, create mask2 and add to outputfile
 if [ $thr2 -gt 0 ]; then
-    bet ${inputfile} ${workingFolder}/mask2.nii.gz -f ${thr2} -g 0 -c ${x2} ${y2} ${z2} -m
+    bet ${inputfile} ${workingFolder}/mask2.nii.gz -f ${thr_2} -g 0 -c ${betx_2} ${bety_2} ${betz_2} -m
     fslmaths ${outputfile} -add ${workingFolder}/mask2.nii.gz ${outputfile}
 fi
 
 # if thr3 > 0, create mask3 and add to outputfile
 if [ $thr3 -gt 0 ]; then
-    bet ${inputfile} ${workingFolder}/mask3.nii.gz -f ${thr3} -g 0 -c ${x3} ${y3} ${z3} -m
+    bet ${inputfile} ${workingFolder}/mask3.nii.gz -f ${thr_3} -g 0 -c ${betx_3} ${bety_3} ${betz_3} -m
     fslmaths ${outputfile} -add ${workingFolder}/mask3.nii.gz ${outputfile}
 fi
--- utils.py
+++ utils.py
@@ -120,7 +120,7 @@ def reorient_file(input_file, output_file, combination):
 
     print('Reorientation done!')
 
-def write_params(filename, x_lim1, y_lim1, z_lim1, x_lim2, y_lim2, z_lim2):
+def write_params(params_file, x_lim1, y_lim1, z_lim1, x_lim2, y_lim2, z_lim2):
     '''
     the function will write a txt file with the limits to be loaded in a bash script
     initialx=x_lim1
@@ -131,7 +131,7 @@ def write_params(filename, x_lim1, y_lim1, z_lim1, x_lim2, y_lim2, z_lim2):
     finalz=z_lim2
     '''
 
-    with open(filename, 'w') as f:
+    with open(params_file, 'w') as f:
         f.write(f'initialx={x_lim1}\n')
         f.write(f'finalx={x_lim2}\n')
         f.write(f'initialy={y_lim1}\n')
@@ -140,3 +140,25 @@ def write_params(filename, x_lim1, y_lim1, z_lim1, x_lim2, y_lim2, z_lim2):
         f.write(f'finalz={z_lim2}\n')
 
     print('cutting parameters file saved as ' + filename)
+
+def write_params_bet(params_file, betx_1, bety_1, betz_1, thr_1, betx_2, bety_2, betz_2, thr_2, betx_3, bety_3, betz_3, thr_3):
+    '''
+    the function will write a txt file with BET parameters to be loaded in a bash script
+    betx_1, bety_1, betz_1, thr_1, betx_2, bety_2, betz_2, thr_2, betx_3, bety_3, betz_3, thr_3
+    '''
+
+    with open(params_file, 'w') as f:
+        f.write(f'betx_1={betx_1}\n')
+        f.write(f'bety_1={bety_1}\n')
+        f.write(f'betz_1={betz_1}\n')
+        f.write(f'thr_1={thr_1}\n')
+        f.write(f'betx_2={betx_2}\n')
+        f.write(f'bety_2={bety_2}\n')
+        f.write(f'betz_2={betz_2}\n')
+        f.write(f'thr_2={thr_2}\n')
+        f.write(f'betx_3={betx_3}\n')
+        f.write(f'bety_3={bety_3}\n')
+        f.write(f'betz_3={betz_3}\n')
+        f.write(f'thr_3={thr_3}\n')
+
+    print('BET parameters file saved as ' + params_file)
